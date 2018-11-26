1866623724499c732706ff427557893e2ddf1ec4
Parker Agee <pagee@pagee.local>
2018-11-26 11:53:24 -0600

    Revert "Log identity"
    This reverts commit 85732fb3a5a90c69dc90bf55caabbe60ec80918d.

--- src/lambda/third-party-api.js
+++ src/lambda/third-party-api.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 
 export function handler(event, context, callback) {
-    const {identity, user} = context.clientContext;
-    console.log('parker', identity, user);
     axios.get('https://dog.ceo/api/breeds/image/random')
         .then(resp => {
             callback(null, {
