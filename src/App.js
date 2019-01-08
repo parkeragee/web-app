e164b69431c9a2ff684e3329b771e7836bdbb51c
Parker Agee <pagee@pagee.local>
2019-01-08 17:04:52 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
 
-import LambdaDemo from './LambdaDemo';
-
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>Jabba boilerplate</h1>
-                <LambdaDemo />
             </div>
         );
     }
